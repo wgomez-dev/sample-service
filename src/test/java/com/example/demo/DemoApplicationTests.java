8f5fed3823c39552070119971e45a394a3f96457
Wilson Gomez <initial.install@8-242.147-129.centurylink.com.co>
2022-05-04 14:11:01 -0500

    add dummy assert true

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,18 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void dummyTest(){
+		assertEquals(1, 1);
 	}
 
 }
